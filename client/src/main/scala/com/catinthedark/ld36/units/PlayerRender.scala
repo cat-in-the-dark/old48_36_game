23258ebc9c8b9713bf08f4022c3fc7b4ad249467
Kirill <kolbasisha@gmail.com>
2016-08-28 13:44:40 +0600

    Intersect players.

--- client/src/main/scala/com/catinthedark/ld36/units/PlayerRender.scala
+++ client/src/main/scala/com/catinthedark/ld36/units/PlayerRender.scala
@@ -1,8 +1,7 @@
 package com.catinthedark.ld36.units
 
-import com.badlogic.gdx.math.Affine2
 import com.catinthedark.ld36.Shared0
-import com.catinthedark.lib.{LocalDeferred, MagicSpriteBatch, SimpleUnit}
+import com.catinthedark.lib.{MagicSpriteBatch, SimpleUnit}
 
 /**
   * Created by kirill on 27.08.16.
