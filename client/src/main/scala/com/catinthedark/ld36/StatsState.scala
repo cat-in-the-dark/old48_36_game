527dcfe186b3618b20f14c011945181cab40e747
Ilya Siganov <ilya.blan4@gmail.com>
2016-08-28 15:02:16 +0600

    add skip stats

--- client/src/main/scala/com/catinthedark/ld36/StatsState.scala
+++ client/src/main/scala/com/catinthedark/ld36/StatsState.scala
@@ -1,7 +1,7 @@
 package com.catinthedark.ld36
 
 
-import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.{Gdx, Input, InputAdapter}
 import com.catinthedark.ld36.common.Stats
 import com.catinthedark.ld36.hud.StatsRender
 import com.catinthedark.lib.YieldUnit
@@ -15,11 +15,22 @@ class StatsState extends YieldUnit[Stats, Unit] {
   var stats: Stats = _
 
 
-  override def onExit() = {}
+  override def onExit() = {
+    Gdx.input.setInputProcessor(null)
+  }
 
   override def onActivate(data: Stats): Unit = {
     stats = data
     stateTime = 0
+    Gdx.input.setInputProcessor(new InputAdapter{
+      override def keyDown(keyCode: Int): Boolean = {
+        keyCode match {
+          case Input.Keys.ENTER => stateTime = delay
+          case _ =>
+        }
+        true
+      }
+    })
   }
 
   override def run(delta: Float): Option[Unit] = {
